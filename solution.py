06aaedddd7d8a3afe7be5a651bb1770ff47f42c1
Dmitry Kostin <112777722+DmitryKostin@users.noreply.github.com>
2023-04-09 21:03:58 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ks_2samp
+from scipy.stats import cramervonmises_2samp
 
 chat_id = 944932368
 
 def solution(x: np.array, y: np.array) -> bool:
     alpha = 0.05
-    statistic, pvalue = ks_2samp(x, y)
-    return pvalue < alpha
+    p_val = cramervonmises_2samp(x, y).pvalue
+    return p_val < alpha
